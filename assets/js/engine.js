b926d8097a8c2468cd2a370cd0bad474da1b62a9
Narto Junior <nartojunior@outlook.com>
2023-12-05 01:57:07 -0300

    feat(game): add finished game

--- assets/js/engine.js
+++ assets/js/engine.js
@@ -1,24 +1,52 @@
 const emojis = [
     "😶‍🌫️", "😶‍🌫️", "🤣", "🤣",
-    "🤡", "🤡", "👾", "👾",
+    "🤡", "🤡", "👽", "👽",
     "🐭", "🐭", "🦈", "🦈",
     "🪸", "🪸", "🧌", "🧌"
 ];
 
-let openedCards = [];
+let openedCards = []
 
-let shuffleEmojis = emojis
+let shuffleEmojis = emojis.sort((a, b) =>
+    Math.random() > 0.5 ? 2 : -1
+)
 
-/*
-let shuffleEmojis = emojis.sort( () =>{
-     Math.random() > 0.5 ? 2 : -1 
-    })
-*/
-for(let i=0; emojis.length; i++){
+shuffleEmojis.forEach((value, i) => {
     let box = document.createElement("div")
     box.className = "item"
-    box.innerHTML = shuffleEmojis[i]
+    box.innerHTML = value
+    box.onclick = handleClick
     document.querySelector(".game").appendChild(box)
+})
+
+function handleClick() {
+    console.log("girando")
+    if (openedCards.length < 2) {
+        this.classList.add("boxOpen")
+        openedCards.push(this);
+    }
+
+    if (openedCards.length === 2) {
+        setTimeout(checkmatch, 500)
+    }
 }
 
-//alert("Eita")
+function checkmatch() {
+    if (openedCards[0].innerHTML === openedCards[1].innerHTML) {
+        openedCards[0].classList.add("boxMatch")
+        openedCards[1].classList.add("boxMatch")        
+    }
+    else {
+        openedCards[0].classList.remove("boxOpen")
+        openedCards[1].classList.remove("boxOpen")        
+    }
+
+    openedCards = []
+
+    if (document.querySelectorAll(".boxOpen").length === shuffleEmojis.length) {
+        alert("Parabéns, você venceu!");
+    }
+}
+
+
+  
